6fae7e8c3ec2f4853d19a57fa0489fb5f7288651
Florentino <florenrey@hotmail.es>
2023-12-09 13:10:49 +0100

    casi

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import random
 import pandas as pd
+from selenium.webdriver.support.ui import Select
 # %%
 # The sleep function can help you to avoid the server 
 # to be overloaded with too many requests in a very short period of time. Basically, with sleep you can make the script stop for a certain period of time so that if you are making some requests iteratively you can avoid the server to be overloaded.
@@ -44,19 +45,18 @@ for name,url in ingesta.items():
     informacionHistorica = driver.find_element(By.XPATH, "//a[contains(text(), 'Información histórica')]")
     driver.execute_script("arguments[0].click();",informacionHistorica)
     sleep()
-    driver.find_element(By.XPATH,'//*[@id="__next"]/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]').click()
-    driver.find_element(By.XPATH,'//*[@id="__next"]/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/div/div[3]/span').click()
+    driver.find_element(By.CLASS_NAME,'historical-data-v2_selection-arrow__3mX7U').click()
+    driver.find_element(By.XPATH, "//span[contains(text(), 'Mensual')]").click()
     sleep()
     tablilla = driver.find_element(By.XPATH,'//*[@id="__next"]/div[2]/div[2]/div[2]/div[1]/div[2]/div[3]/table').get_attribute('outerHTML')
     df = pd.read_html(tablilla)[0]
     df["Nombre"] = (name) * len(df)
     dataframeBusines = pd.concat([dataframeBusines,df])
-    driver.find_element(By.XPATH,'//*[@id="__next"]/div[2]/div[2]/div[2]/div[1]/nav/div[1]/ul/li[3]/ul/li[1]/a').click()
-    driver.find_element(By.XPATH,'//*[@id="__next"]/div[2]/div[2]/div[2]/div[1]/div[4]/div/div[1]/div/button[8]').click()
+    driver.find_element(By.XPATH,'//*[@id="__next"]/div[2]/div[2]/div[2]/div[1]/nav/div[1]/ul/li[4]').click()
+    driver.find_element(By.XPATH, "//span[contains(text(), 'Mensual')]").click()
     ind_tecnico = driver.find_element(By.XPATH,'//*[@id="__next"]/div[2]/div[2]/div[2]/div[1]/div[4]/div/div[2]/div[2]/div[2]/div/table').get_attribute('outerHTML')
-    df_ind_tecnico_tabla = pd.read_html(ind_tecnico)[0]
-    df_ind_tecnico_tabla["Nombre"] = (name) * len(df)
-    df_ind_tecnico_tabla["Fecha"] = (df['fecha']) * len(df)
-    dataframe_ind_tecnico = pd.concat([dataframe_ind_tecnico,df_ind_tecnico_tabla])
+    df_ind_tecnico_tabla_mensual = pd.read_html(ind_tecnico)[0]
+    df_ind_tecnico_tabla_mensual["Nombre_emp"] = (name) * len(df)
+    dataframe_ind_tecnico = pd.concat([dataframe_ind_tecnico,df_ind_tecnico_tabla_mensual])
 print(dataframe1)
 # %%
